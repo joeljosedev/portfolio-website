8bc96cb9e079fa00e67c4fd161dc7480ce814446
Joel Jose <mail@joeljose.dev>
2022-05-23 17:33:25 +0530

    Implemented Navbar for wide screens (desktop)

--- components/NavbarDesktop.tsx
+++ components/NavbarDesktop.tsx
@@ -1,7 +1,50 @@
-const NavbarDesktop = () => {
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/NavbarDesktop.module.css";
+import { NavbarPropsType } from "./Navbar";
+
+const NavbarDesktop = (props: NavbarPropsType) => {
   return (
-    <div>
-      <div>NavbarDesktop</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image
+          src="/android-chrome-512x512.png"
+          alt="Logo"
+          width={512}
+          height={512}
+        />
+      </div>
+      <div className={styles.name}>Joel Jose</div>
+      <Link href="/">
+        <div
+          className={props.active === "home" ? styles.activeTab : styles.tab}
+        >
+          Home
+        </div>
+      </Link>
+      <Link href="/about">
+        <div
+          className={props.active === "about" ? styles.activeTab : styles.tab}
+        >
+          About
+        </div>
+      </Link>
+      <Link href="/projects">
+        <div
+          className={
+            props.active === "projects" ? styles.activeTab : styles.tab
+          }
+        >
+          Projects
+        </div>
+      </Link>
+      <Link href="/contact">
+        <div
+          className={props.active === "contact" ? styles.activeTab : styles.tab}
+        >
+          Contact
+        </div>
+      </Link>
     </div>
   );
 };
