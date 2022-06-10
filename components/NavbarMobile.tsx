d87eb8460bfa628b8b52e2b5d5f9b108c6dfa127
Joel Jose <mail@joeljose.dev>
2022-06-10 18:31:10 +0530

    Implemented Navbar for small screens (mobile)

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import styles from "../styles/NavbarMobile.module.css";
+import MenuIcon from "./MenuIcon";
+
 const NavbarMobile = () => {
   return (
-    <div>
-      <div>NavbarMobile</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image
+          src="/android-chrome-512x512.png"
+          alt="Logo"
+          width={512}
+          height={512}
+        />
+      </div>
+      <div className={styles.name}>Joel Jose</div>
+      <div className={styles.menu}>
+        <MenuIcon />
+      </div>
     </div>
   );
 };
