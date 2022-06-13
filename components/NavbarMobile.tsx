437c3d49a65931dd56a44f86577e315a24cf6437
Joel Jose <mail@joeljose.dev>
2022-06-14 00:43:27 +0530

    Implemented Page Modal for small screens (mobile)

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,23 +1,35 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/NavbarMobile.module.css";
 import MenuIcon from "./MenuIcon";
+import { NavbarPropsType } from "./Navbar";
+import PageModal from "./PageModal";
 
-const NavbarMobile = () => {
+const NavbarMobile = (props: NavbarPropsType) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <Image
-          src="/android-chrome-512x512.png"
-          alt="Logo"
-          width={512}
-          height={512}
-        />
+    <>
+      <PageModal
+        active={props.active}
+        setShowModal={setShowModal}
+        showModal={showModal}
+      />
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image
+            src="/android-chrome-512x512.png"
+            alt="Logo"
+            width={512}
+            height={512}
+          />
+        </div>
+        <div className={styles.name}>Joel Jose</div>
+        <div className={styles.menu} onClick={() => setShowModal(true)}>
+          <MenuIcon />
+        </div>
       </div>
-      <div className={styles.name}>Joel Jose</div>
-      <div className={styles.menu}>
-        <MenuIcon />
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/PageModal.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import styles from "../styles/PageModal.module.css";
+import CloseIcon from "./CloseIcon";
+
+interface ModalPropsType {
+  active: string;
+  setShowModal: any;
+  showModal: boolean;
+}
+
+const PageModal = (props: ModalPropsType) => {
+  if (props.showModal) {
+    document.body.style.overflow = "hidden";
+
+    const handleClick = (event: any) => {
+      const objectId = event.currentTarget.id;
+
+      if (objectId === "container") {
+        props.setShowModal(false);
+      } else {
+        event.stopPropagation();
+      }
+    };
+
+    return (
+      <div
+        id="container"
+        className={styles.container}
+        onClick={(event) => handleClick(event)}
+      >
+        <div
+          id="modal"
+          className={styles.modal}
+          onClick={(event) => handleClick(event)}
+        >
+          <div
+            className={styles.close}
+            onClick={() => props.setShowModal(false)}
+          >
+            <CloseIcon />
+          </div>
+          <Link href="/">
+            <div
+              className={
+                props.active === "home" ? styles.activeTab : styles.tab
+              }
+              onClick={() => props.setShowModal(false)}
+            >
+              Home
+            </div>
+          </Link>
+          <Link href="/about">
+            <div
+              className={
+                props.active === "about" ? styles.activeTab : styles.tab
+              }
+              onClick={() => props.setShowModal(false)}
+            >
+              About
+            </div>
+          </Link>
+          <Link href="/projects">
+            <div
+              className={
+                props.active === "projects" ? styles.activeTab : styles.tab
+              }
+              onClick={() => props.setShowModal(false)}
+            >
+              Projects
+            </div>
+          </Link>
+          <Link href="/contact">
+            <div
+              className={
+                props.active === "contact" ? styles.activeTab : styles.tab
+              }
+              onClick={() => props.setShowModal(false)}
+            >
+              Contact
+            </div>
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    document.body.style.overflow = "scroll";
+
+    return <></>;
+  }
+};
+
+export default PageModal;
