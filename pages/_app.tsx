d2f30d107b8196ba38df7442bcd998cbb5462e50
Joel Jose <mail@joeljose.dev>
2022-06-25 23:52:53 +0530

    Applied RotateDevice component to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import RotateDevice from "../components/RotateDevice";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <RotateDevice />
     <Navbar />
     <Component {...pageProps} />
   </>
