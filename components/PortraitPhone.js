0ab73dee404c792954fad05745912096d6ec7a0f
Joel Jose <mail@joeljose.dev>
2022-06-25 23:40:07 +0530

    Updated portrait phone icon styles

--- components/PortraitPhone.js
+++ components/PortraitPhone.js
@@ -2,11 +2,11 @@ const PortraitPhone = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
+      className="h-40 w-40"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      strokeWidth={2}
+      strokeWidth={1}
     >
       <path
         strokeLinecap="round"
