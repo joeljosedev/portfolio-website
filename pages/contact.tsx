eea173bef7376b1f6c28d611ac54978c9c08ab1d
Joel Jose <mail@joeljose.dev>
2022-06-26 21:10:04 +0530

    Implemented Contact Page

---
+++ pages/contact.tsx
@@ -0,0 +1,34 @@
+import styles from "../styles/contact.module.css";
+
+const Contact = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>My Accounts</div>
+      <div className={styles.cardContainer}>
+        <a href="https://wa.me/+919846642788">
+          <div className={styles.card}>WhatsApp</div>
+        </a>
+        <a href="https://www.linkedin.com/in/joeljosedev">
+          <div className={styles.card}>LinkedIn</div>
+        </a>
+        <a href="https://github.com/joeljosedev">
+          <div className={styles.card}>GitHub</div>
+        </a>
+        <a href="https://t.me/joeljosedev">
+          <div className={styles.card}>Telegram</div>
+        </a>
+      </div>
+      <div className={styles.title}>Contact Me</div>
+      <div className={styles.cardContainer}>
+        <a href="tel:+919846642788">
+          <div className={styles.card}>Phone</div>
+        </a>
+        <a href="mailto:mail@joeljose.dev">
+          <div className={styles.card}>Email</div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
