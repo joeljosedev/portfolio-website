41da226f12fd077b9e40fb698636f5005fd716e2
Joel Jose <mail@joeljose.dev>
2022-06-26 21:55:08 +0530

    Fixed broken link to Resume

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
         <a href="mailto:mail@joeljose.dev">
           <div className={styles.card}>Email</div>
         </a>
-        <a href="/resume.pdf" target="_blank">
+        <a href="/Joel-Jose-Resume.pdf" target="_blank">
           <div className={styles.card}>Resume</div>
         </a>
       </div>
