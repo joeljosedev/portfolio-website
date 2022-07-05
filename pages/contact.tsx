c742c4489b8841a8d5aef4e8dd3a61ae48ec55f7
Joel Jose <mail@joeljose.dev>
2022-07-05 20:10:19 +0530

    Added rel="noreferrer" to <a> tags

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,25 +5,33 @@ const Contact = () => {
     <div className={styles.container}>
       <div className={styles.title1}>My Accounts</div>
       <div className={styles.cardContainer}>
-        <a href="https://wa.me/+919846642788" target="_blank">
+        <a href="https://wa.me/+919846642788" target="_blank" rel="noreferrer">
           <div className={styles.card1}>WhatsApp</div>
         </a>
-        <a href="https://www.linkedin.com/in/joeljosedev" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/joeljosedev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className={styles.card2}>LinkedIn</div>
         </a>
-        <a href="https://github.com/joeljosedev" target="_blank">
+        <a
+          href="https://github.com/joeljosedev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className={styles.card3}>GitHub</div>
         </a>
-        <a href="https://t.me/joeljosedev" target="_blank">
+        <a href="https://t.me/joeljosedev" target="_blank" rel="noreferrer">
           <div className={styles.card4}>Telegram</div>
         </a>
       </div>
       <div className={styles.title2}>Contact Me</div>
       <div className={styles.cardContainer}>
-        <a href="mailto:mail@joeljose.dev" target="_blank">
+        <a href="mailto:mail@joeljose.dev" target="_blank" rel="noreferrer">
           <div className={styles.card5}>Email</div>
         </a>
-        <a href="/Joel-Jose-Resume.pdf" target="_blank">
+        <a href="/Joel-Jose-Resume.pdf" target="_blank" rel="noreferrer">
           <div className={styles.card6}>Resume</div>
         </a>
       </div>
