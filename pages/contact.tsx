c98d30cc63bd42268cf4a95f881663bab3b5e86e
Joel Jose <mail@joeljose.dev>
2022-07-05 19:37:00 +0530

    Added target="_blank" to links in Contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,22 +5,22 @@ const Contact = () => {
     <div className={styles.container}>
       <div className={styles.title1}>My Accounts</div>
       <div className={styles.cardContainer}>
-        <a href="https://wa.me/+919846642788">
+        <a href="https://wa.me/+919846642788" target="_blank">
           <div className={styles.card1}>WhatsApp</div>
         </a>
-        <a href="https://www.linkedin.com/in/joeljosedev">
+        <a href="https://www.linkedin.com/in/joeljosedev" target="_blank">
           <div className={styles.card2}>LinkedIn</div>
         </a>
-        <a href="https://github.com/joeljosedev">
+        <a href="https://github.com/joeljosedev" target="_blank">
           <div className={styles.card3}>GitHub</div>
         </a>
-        <a href="https://t.me/joeljosedev">
+        <a href="https://t.me/joeljosedev" target="_blank">
           <div className={styles.card4}>Telegram</div>
         </a>
       </div>
       <div className={styles.title2}>Contact Me</div>
       <div className={styles.cardContainer}>
-        <a href="mailto:mail@joeljose.dev">
+        <a href="mailto:mail@joeljose.dev" target="_blank">
           <div className={styles.card5}>Email</div>
         </a>
         <a href="/Joel-Jose-Resume.pdf" target="_blank">
