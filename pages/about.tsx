fea94c249e1d25748083c55fa4e15153f2b9e334
Joel Jose <mail@joeljose.dev>
2022-11-24 22:11:11 +0530

    Rephrased about section

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,16 +6,16 @@ const About: NextPage = () => {
     <div className={styles.container}>
       <div className={styles.title}>About Me</div>
       <div id={styles.para1}>
-        I am a software developer based in Kochi, India and my areas of
-        expertise include web development, Linux system administration and
-        configuring SOHO network infrastructure.
+        I am a software developer based in Kochi, India. My areas of expertise
+        include web development, Linux system administration and configuring
+        SOHO network infrastructure.
       </div>
       <div id={styles.para2}>
         I also have experience creating and configuring Windows and Linux
         virtual machines on VMware Workstation and Oracle VirtualBox.
       </div>
       <div id={styles.para3}>
-        Languages, tools and frameworks I am proficient in:
+        Programming languages, frameworks and tools I am proficient in:
       </div>
       <ul id={styles.list}>
         <li>Next.js</li>
