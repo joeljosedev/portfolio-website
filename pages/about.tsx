d4a29ca978cdcff7c50fbbe95f14f9c375fb1791
Joel Jose <mail@joeljose.dev>
2022-12-09 22:46:57 +0530

    Changed CSS ids to classes

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,19 +5,19 @@ const About: NextPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>About Me</div>
-      <div id={styles.para1}>
+      <div className={styles.para1}>
         I am a software developer based in Kochi, India. My areas of expertise
         include web development, Linux system administration and configuring
         SOHO network infrastructure.
       </div>
-      <div id={styles.para2}>
+      <div className={styles.para2}>
         I also have experience creating and configuring Windows and Linux
         virtual machines on VMware Workstation and Oracle VirtualBox.
       </div>
-      <div id={styles.para3}>
+      <div className={styles.para3}>
         Programming languages, frameworks and tools I am proficient in:
       </div>
-      <ul id={styles.list}>
+      <ul className={styles.list}>
         <li>Next.js</li>
         <li>Tailwind CSS</li>
         <li>TypeScript</li>
