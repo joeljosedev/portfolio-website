423e45fcad1a8d5cfc5d8a82f0e0df372b353416
Joel Jose <mail@joeljose.dev>
2022-12-13 20:28:12 +0530

    Removed logo from Navbars

--- components/NavbarDesktop.tsx
+++ components/NavbarDesktop.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/NavbarDesktop.module.css";
 import { NavbarPropsType } from "./Navbar";
@@ -7,17 +6,7 @@ const NavbarDesktop = (props: NavbarPropsType) => {
   return (
     <div className={styles.container}>
       <Link href="/">
-        <div className={styles.linkContainer}>
-          <div className={styles.logo}>
-            <Image
-              src="/android-chrome-512x512.png"
-              alt="Logo"
-              width={512}
-              height={512}
-            />
-          </div>
-          <div className={styles.name}>Joel Jose</div>
-        </div>
+        <div className={styles.name}>Joel Jose</div>
       </Link>
       <Link href="/">
         <div
--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import MenuIcon from "../icons/MenuIcon";
@@ -18,17 +17,7 @@ const NavbarMobile = (props: NavbarPropsType) => {
       />
       <div className={styles.container}>
         <Link href="/">
-          <div className={styles.linkContainer}>
-            <div className={styles.logo}>
-              <Image
-                src="/android-chrome-512x512.png"
-                alt="Logo"
-                width={512}
-                height={512}
-              />
-            </div>
-            <div className={styles.name}>Joel Jose</div>
-          </div>
+          <div className={styles.name}>Joel Jose</div>
         </Link>
         <div className={styles.menu} onClick={() => setShowModal(true)}>
           <MenuIcon />
